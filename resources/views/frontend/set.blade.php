fb667ade0fbbd0ade2cd76422ac58f6415a048c2
smilezdev <vixxjy@gmail.com>
2018-03-17 15:30:13 +0000

    game

--- resources/views/frontend/set.blade.php
+++ resources/views/frontend/set.blade.php
@@ -1,1 +1,12 @@
-happy
+@extends('layout.master')
+
+@section('title')
+ co-operate loans dashboard
+@endsection
+
+@section('content')
+ @hasanyrole('Admin|Staff')
+          happy
+        @endhasanyrole
+@endsection
+
